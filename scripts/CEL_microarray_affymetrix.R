22fc02622496a18cad44a31b8bcb6237136216d6
algolikitha28 <dklikitha@gmail.com>
2026-05-13 10:36:49 +0530

    Added gene co-expression network and gene ID mapping

--- scripts/CEL_microarray_affymetrix.R
+++ scripts/CEL_microarray_affymetrix.R
@@ -105,3 +105,75 @@ save(
   model,
   file = "geNetClassifier_model.RData"
 )
+
+#network construction
+
+#install packages
+install.packages("igraph")
+install.packages("ggraph")
+install.packages("tidygraph")
+
+#load 
+library(igraph)
+library(ggraph)
+library(tidygraph)
+
+#select top genes
+topgenes <- rownames(results)[1:20]
+network_data <- expr[topgenes, ]
+
+#calculate corelation matrix
+cor_matrix <- cor(
+  t(network_data),
+  method = "pearson"
+)
+
+#remove weak corelations
+cor_matrix[abs(cor_matrix) < 0.8] <- 0
+
+#create network
+library(igraph)
+network <- graph_from_adjacency_matrix(
+  cor_matrix,
+  mode = "undirected",
+  weighted = TRUE,
+  diag = FALSE
+)
+
+#plot network
+plot(
+  network,
+  vertex.size = 20,
+  vertex.label.cex = 0.8,
+  main = "Gene Co-expression Network"
+)
+
+#instal annotation package
+BiocManager::install("hgu95av2.db")
+
+#load
+library(hgu95av2.db)
+library(AnnotationDbi)
+
+#Get Probe IDs from Network
+probe_ids <- V(network)$name
+
+#map to gene symbols
+gene_symbols <- mapIds(
+  hgu95av2.db,
+  keys = probe_ids,
+  column = "SYMBOL",
+  keytype = "PROBEID",
+  multiVals = "first"
+)
+
+#add labels to network
+V(network)$label <- gene_symbols
+
+#plot network
+plot(
+  network,
+  vertex.size = 20,
+  vertex.label.cex = 0.7,
+  main = "Gene Co-expression Network"
+)
